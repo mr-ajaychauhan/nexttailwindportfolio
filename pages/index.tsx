7ffb6558ed00e7825ccf1dcaef147c7352b327e3
Ajay Chauhan <48796206+mr-ajaychauhan@users.noreply.github.com>
2023-04-17 18:03:49 +0000

    try making

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { BsFillBrightnessHighFill } from 'react-icons/bs'
+import {AiFillTwitterCircle ,AiFillLinkedin,AiFillYoutube} from 'react-icons/ai'
 
 export default function Home() {
   return (
@@ -17,14 +18,35 @@ export default function Home() {
             <ul className="flex items-center">
               <li>
                 <BsFillBrightnessHighFill className="cursor-pointer text-2xl" />
+
               </li>
               <li>
-                <Link href='#' className="bg-gradient-to-r from-cyan-700 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8">
+                <Link href='#' className="
+                bg-gradient-to-r 
+                from-cyan-700 
+                to-teal-500 
+                text-white 
+                px-4 
+                py-2 
+                border-none 
+                rounded-md 
+                ml-8
+                
+                "
+                >
                   Resume
                 </Link>
               </li>
             </ul>
           </nav>
+          <div>
+              <h2>Ajay Chauhan</h2>
+              <h3>Software Engineer</h3>
+              <p>hi I am Software engineer i am a lead front end engineer and a ios Developer.</p>
+          </div>
+          <div>
+
+          </div>
         </section>
       </main>
     </div>
